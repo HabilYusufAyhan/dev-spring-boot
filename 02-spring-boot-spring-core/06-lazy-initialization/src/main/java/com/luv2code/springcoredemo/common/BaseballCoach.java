4834038490b4e2df13cc12cda8c4ea21141cd872
Habil Yusuf Ayhan <habilyusuf20@gmail.com>
2024-07-02 14:14:17 +0300

    lazy initialization

---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,18 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy // Class ihtiyaç duyulmadığı sürece yaratılmaz
+public class BaseballCoach implements Coach{
+
+    public BaseballCoach(){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+    @Override
+    public String getDailyWorkout() {
+
+        return "Spring 30 minutes in batting practice";
+    }
+}
